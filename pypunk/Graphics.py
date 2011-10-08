e642a7e21b26b785e15175f9c8844c99c68c4b6e
Saxon Landers <info.clockworkgames@gmail.com>
2011-10-08 11:04:57 +1100

    Merged Graphic.py -> Graphics.py

--- pypunk/Graphics.py
+++ pypunk/Graphics.py
@@ -1,5 +1,5 @@
 from PySFML import sf
-from Graphic import Graphic
+from Punk import Point
 
 #Reference to colour
 Color = sf.Color
@@ -8,6 +8,55 @@ class Rectangle:
 	def __init__(self, x, y, width, height):
 		self.x = x; self.y = y;
 		self.width = width; self.height = height;
+
+
+
+class Graphic(object):
+	def __init__(self):
+		# If the graphic should update
+		self.active = False
+
+		# if the graphic should render
+		self.visible = True
+
+		# X offset
+		self.x = 0
+
+		# Y offset
+		self.y = 0
+
+		# If the graphic should render at its position relative to its parent Entity's position.
+		self.relative = True
+
+		# X scrollfactor, effects how much the camera offsets the drawn graphic.
+		# Can be used for parallax effect, eg. Set to 0 to follow the camera,
+		# 0.5 to move at half-speed of the camera, or 1 (default) to stay still.
+		self.scollX = 1
+
+		# Y scrollfactor, effects how much the camera offsets the drawn graphic.
+		#  Can be used for parallax effect, eg. Set to 0 to follow the camera,
+		# 0.5 to move at half-speed of the camera, or 1 (default) to stay still.
+		self.scrollY = 1
+
+		# Graphic information
+		self._assign = None		# private
+		self._scroll = True		# private
+		self._point = Point()	# private
+
+
+	def update(self):
+		pass
+
+	def render(self, target, point, camera):
+		pass
+
+	@property
+	def assign(self):
+		return self._assign
+
+	@assign.setter
+	def assign(self, value):
+		self._assign = value
 
 class Image(Graphic, sf.Sprite):
 	def __init__(self, loc, rect=None):
